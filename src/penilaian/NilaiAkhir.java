6110e5d3a2c73ee99a5b7b80f1bba7e8a1ba3fd8
SyabanFirmansyah <lakaniman42@gmail.com>
2023-06-19 17:10:34 +0800

    membuat program latihan 4

--- src/penilaian/NilaiAkhir.java
+++ src/penilaian/NilaiAkhir.java
@@ -1,32 +1,39 @@
 package penilaian;
 
 public class NilaiAkhir extends Mahasiswa {
-    protected int uts, uas, tugas;
-
-    public NilaiAkhir(int uts, int uas, int tugas) {
+    protected double uts, uas, tugas;
+    
+    public double getUts() {
+        return uts;
+    }
+    
+    public void setUts(double uts) {
         this.uts = uts;
+    }
+    
+    public double getUas() {
+        return uas;
+    }
+    
+    public void setUas(double uas) {
         this.uas = uas;
+    }
+    
+    public double getTugas() {
+        return tugas;
+    }
+    
+    public void setTugas(double tugas) {
         this.tugas = tugas;
     }
-
-    NilaiAkhir() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
     
-    int hitungNilaiAkhir(){
-        return 30/100*uts+30/100*uas+40/100+tugas;
-    }
-    
-    @Override
-    public String getNama(){
-        System.out.println("subclass");
-        return super.getNama();
-    }
-    
-    @Override
-    public String getNpm(){
-        System.out.println("subclass");
-        return super.getNpm();
+    public double hitungNilaiAkhir() {
+        // Menggunakan bobot 40% UTS, 40% UAS, dan 20% tugas
+        double bobotUts = 0.4;
+        double bobotUas = 0.4;
+        double bobotTugas = 0.2;
+        
+        return (uts * bobotUts) + (uas * bobotUas) + (tugas * bobotTugas);
     }
     
 }
--- src/penilaian/main.java
+++ src/penilaian/main.java
@@ -2,12 +2,23 @@ package penilaian;
 
 public class main {
     public static void main(String[] args) {
-        NilaiAkhir iman1 = new NilaiAkhir(80,80,90);
+         //objek mahasiswa
+        Mahasiswa mahasiswa = new Mahasiswa();
+        mahasiswa.setNama("Sya'ban Firmansyah");
+        mahasiswa.setNpm("2110010280");
         
-        iman1.setNama("Syaban Firmansyah");
-        iman1.setNpm("2110010280");
+        //objek NilaiAkhir
+        NilaiAkhir nilaiAkhir = new NilaiAkhir();
+        nilaiAkhir.setUts(90);
+        nilaiAkhir.setUas(85);
+        nilaiAkhir.setTugas(99);
         
-        System.out.println("nama        :"+iman1.getNama());
-        System.out.println("npm         :"+iman1.getNpm());
+        // tampilan data
+        System.out.println("Nama Mahasiswa: " + mahasiswa.getNama());
+        System.out.println("NPM Mahasiswa: " + mahasiswa.getNpm());
+        System.out.println("Nilai UTS: " + nilaiAkhir.getUts());
+        System.out.println("Nilai UAS: " + nilaiAkhir.getUas());
+        System.out.println("Nilai Tugas: " + nilaiAkhir.getTugas());
+        System.out.println("Nilai Akhir: " + nilaiAkhir.hitungNilaiAkhir());
     }
 }
